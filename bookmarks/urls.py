955108ae4bb59bc5d2b004a46b492be8d06d7b71
Ting Wang <felix.wang.1026@gmail.com>
2018-04-26 22:21:12 -0400

    added crud for bookmarks app

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update/<bookmark_id>', views.update, name='update'),
+    path('delete/<bookmark_id>', views.delete, name='delete')
 ]
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import BookmarkForm
 from .models import Bookmark
+from django.http import HttpResponseRedirect
 
 
 def index(request):
@@ -8,5 +9,25 @@ def index(request):
         form = BookmarkForm(request.POST)
         if form.is_valid():
             form.save()
-    context = {'bookmarks': Bookmark.objects.all(), 'form': BookmarkForm()}
+    context = {'bookmarks': Bookmark.objects.all(
+    ), 'form': BookmarkForm(), 'action': 'Create', 'button': 'Create'}
     return render(request, 'bookmarks/index.html', context)
+
+
+def update(request, bookmark_id):
+    bookmark = Bookmark.objects.get(pk=bookmark_id)
+    if request.method == 'POST':
+        form = BookmarkForm(request.POST, instance=bookmark)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/bookmarks')
+    context = {'bookmarks': Bookmark.objects.all(
+    ), 'form': BookmarkForm(), 'action': 'Update', 'button': 'Update'}
+    return render(request, 'bookmarks/index.html', context)
+
+
+def delete(request, bookmark_id):
+    bookmark = Bookmark.objects.get(pk=bookmark_id)
+    if bookmark:
+        bookmark.delete()
+        return HttpResponseRedirect('/bookmarks')
